df0010d70d46775e0d2e48233d3e4503acf73dc7
strayer_practice@outlook.com <strayer_practice@outlook.com>
2023-02-10 13:11:47 -0800

    Amy Giordano

--- Amy_Giordano_CIS261_InvoiceLineItem.py
+++ Amy_Giordano_CIS261_InvoiceLineItem.py
@@ -1,1 +1,29 @@
+def get_price():
+    while True:
+        try:
+            price = float(input("Enter the price: "))
+            return price
+        except ValueError:
+            print("Invalid price. Please enter a valid number.")
+
+def get_quantity():
+    while True:
+        try:
+            quantity = int(input("Enter the quantity: "))
+            return quantity
+        except ValueError:
+            print("Invalid quantity. Please enter a valid integer.")
+
+total = 0
+while True:
+    price = get_price()
+    quantity = get_quantity()
+    line_total = price * quantity
+    total += line_total
+    print("Price: $%.2f\tQuantity: %d\tLine Total: $%.2f" % (price, quantity, line_total))
+    another = input("Do you want to enter another line item? (y/n)").lower()
+    if another == "n":
+        break
+
+print("Total: $%.2f" % total)
 
